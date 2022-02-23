a3f231819ed76f0c031aef73db056f0fe14398ca
TomJZ <zjh@seas.upenn.edu>
2022-02-22 20:01:57 -0500

    added evaluation script

--- evaluation.py
+++ evaluation.py
@@ -1,155 +1,45 @@
-import tqdm
-import numpy as np
-from torch import Tensor
-import torch
-import matplotlib.pyplot as plt
+import time
+from Utils.Plotters import *
+from NODE.NODE import *
 
-mode = "full"  # whether the model is the simplied planar model
+if __name__ == "__main__":
+    """
+    load pretrained model
+    """
+    ode_train = torch.load("SavedModels/vortex_conv_gaussian.pth",
+                           map_location=torch.device('cpu'))['ode_train']
 
-data_path = "Data/uncertain_params/traj_data_mpc_aerodrag_radius3.npy"
-model_path = "SavedModels/full_train_on_2traj.pth"
+    """
+    Load initial condition from data 
+    """
+    snapshot = 200  # the snapshot to use as initial condition
+    processed_data = np.load("Data/Processed/vortex_re200_no_turbulence.npy")
+    grid = np.load("Data/Processed/regularized_grid.npy")
+    X = grid[0, 0, :, :]
+    Y = grid[1, 0, :, :]
+    initial_condition = torch.tensor(processed_data[snapshot]).unsqueeze(0)
+    print(initial_condition.shape)
 
-torch_model = torch.load(model_path, map_location=torch.device('cpu'))
-ode_train = torch_model['ode_train']
-device = torch.device('cpu')
-ode_train.func.device = device
-train_loss_arr = torch_model['train_loss_arr']
-print("Final loss is: \n", train_loss_arr[-1])
+    """
+    Generating predictions
+    """
+    test_len = 50
+    step_skip = 5
+    start = time.time()
+    z_p = ode_train(initial_condition,
+                    step_skip * torch.tensor(np.arange(test_len).astype(int)),
+                    return_whole_sequence=True)
+    duration = time.time() - start
+    print("Total time taken to make prediction is: ", duration, " seconds")
+    pred = z_p.detach().numpy()[:, 0, :, :]
+    print("Prediction has shape: ", pred.shape)
+    V_x = pred[:, 0, :, :]
+    SAVE_ANIM = True
+    SAVE_PLOT = None
+    t0 = 0
+    tN = t0 + test_len
 
-if mode!="planar": 
-    val_loss_arr = torch_model['val_loss_arr']
-
-
-if mode=="rigid":
-    with open(data_path, 'rb') as f:
-        train_set = np.load(f)
-    set_len = train_set.shape[0]
-    train_set = train_set.reshape([set_len, -1])
-    train_set = train_set[:, :]
-    data_length = set_len  # the length of data to keep
-
-    titles = ["x", "y", "z", "x_dot", "y_dot", "z_dot", "x_ddot", "y_ddot", "z_ddot"]
-
-    def plot_all(train_set, new_set=None):
-        fig = plt.figure(figsize=(15, 15))
-        for i in range(9):
-            ax = fig.add_subplot(3, 3, i + 1)
-            ax.plot(train_set[:, i])
-            if new_set is not None:
-                ax.plot(new_set[:, i])
-            ax.set_title(titles[i])
-        plt.savefig("SavedModels/hybrid_state.png", format='png', dpi=400, bbox_inches='tight', pad_inches=0.1)
-
-    plot_len = set_len - 1
-    
-    ode_input = Tensor(train_set[0]).view(1, 9).to(device)
-    pred_traj = [ode_input]
-    print("Generating Predictions")
-    for i in tqdm.tqdm(range(plot_len)):
-        z1 = ode_train(ode_input, Tensor(np.arange(2))).squeeze(1)
-        ode_input = torch.cat([z1[:, :6], Tensor(train_set[i + 1, 6:]).unsqueeze(0).to(device)], 1)
-        pred_traj.append(ode_input)
-
-    pred_traj = torch.cat(pred_traj).cpu().detach().numpy()
-    # plotting the trajectories
-    plot_all(train_set[:plot_len], pred_traj)
-    # plotting the loss array
-
-    fig1 = plt.figure()
-    ax = fig1.add_subplot(1, 1, 1)
-    ax.plot(train_loss_arr[100:], label='Training loss')
-    ax.plot(val_loss_arr[100:], label='Validation loss')
-    ax.legend()
-    plt.savefig("SavedModels/hybrid_loss.png", format='png', dpi=400, bbox_inches='tight', pad_inches=0.1)
-
-elif mode=="planar":
-    with open(data_path, 'rb') as f:
-        train_set = np.load(f)
-    set_len = train_set.shape[0]
-    train_set = train_set.reshape([set_len, -1])
-    train_set = train_set[:, :]
-    data_length = set_len  # the length of data to keep
-
-    titles = ["y", "z", "roll angle", "yd", "zd", "roll rate", "thrust", "moment"]
-
-    def plot_all(train_set, new_set=None):
-        fig = plt.figure(figsize=(25, 5))
-        for i in range(8):
-            ax = fig.add_subplot(2, 4, i + 1)
-            ax.plot(train_set[:, i])
-            if new_set is not None:
-                ax.plot(new_set[:, i])
-            ax.set_title(titles[i])
-        plt.savefig("SavedModels/simple_aerodrag_state.png", format='png', dpi=400, bbox_inches='tight', pad_inches=0.1)
-
-
-    plot_len = set_len - 1
-    
-    ode_input = Tensor(train_set[0]).view(1, 8).to(device)
-    pred_traj = [ode_input]
-    for i in range(plot_len):
-        z1 = ode_train(ode_input, Tensor(np.arange(2))).squeeze(1)
-        ode_input = torch.cat([z1[:, :6], Tensor(train_set[i + 1, 6:]).unsqueeze(0).to(device)], 1)
-        pred_traj.append(ode_input)
-
-    pred_traj = torch.cat(pred_traj).cpu().detach().numpy()
-    # plotting the trajectories
-    plot_all(train_set[:plot_len], pred_traj)
-    # plotting the loss array
-
-    fig1 = plt.figure()
-    ax = fig1.add_subplot(1, 1, 1)
-    ax.plot(train_loss_arr[100:], label='Training loss')
-    ax.plot(val_loss_arr[100:], label='Validation loss')
-    ax.legend()
-    plt.savefig("SavedModels/simple_aerodrag_loss.png", format='png', dpi=400, bbox_inches='tight', pad_inches=0.1)
-    plt.show()
-elif mode=="full":
-    with open(data_path, 'rb') as f:
-        train_set = np.load(f)
-    set_len = train_set.shape[0]
-    train_set = train_set.reshape([set_len, -1])
-    train_set = train_set[::1, 1:18]
-    set_len = train_set.shape[0]
-    data_length = set_len # the length of data to keep
-
-    def plot_all(train_set, new_set=None):
-        titles = ["pos_x", "pos_y", "pos_z",
-                  "v_x", "v_y", "v_z",
-                  "q1", "q2", "q3", "q4",
-                  "w_x", "w_y", "w_z", "thrust",
-                  "mom_x", "mom_y", "mom_z"]
-
-        fig = plt.figure(figsize=(25,5))
-        for i in range(17):
-            ax = fig.add_subplot(2,9,i+1)
-            ax.plot(train_set[:,i])
-            if new_set is not None:
-                ax.plot(new_set[:, i])
-            ax.set_title(titles[i])
-        
-        plt.savefig("SavedModels/3layer_aerodrag_state.png", format='png', dpi=400, bbox_inches='tight', pad_inches=0.1)
-    
-    plot_len = set_len-1
-    
-    ode_input = Tensor(train_set[0]).view(1, 17).to(device)
-    pred_traj = [ode_input]
-    print("Generating Trajectories")
-    for i in tqdm.tqdm(range(plot_len)):
-        z1 = ode_train(ode_input, Tensor(np.arange(2))).squeeze(1)
-        ode_input = torch.cat([z1[:,:13], Tensor(train_set[i+1, 13:]).unsqueeze(0).to(device)],1)
-        pred_traj.append(ode_input)
-
-    pred_traj = torch.cat(pred_traj).cpu().detach().numpy()
-    # plotting the trajectories
-    plot_all(train_set[:plot_len], pred_traj)
-    # plotting the loss array
-
-    fig1 = plt.figure()
-    ax = fig1.add_subplot(1, 1, 1)
-    ax.plot(train_loss_arr[100:], label='Training loss')
-    ax.plot(val_loss_arr[100:], label='Validation loss')
-    ax.legend()
-    plt.savefig("SavedModels/3layer_aerodrag_loss.png", format='png', dpi=400, bbox_inches='tight', pad_inches=0.1)
-    plt.show()
-
+    print(X.shape, V_x.shape)
+    anim = make_flow_anim(X.reshape(-1), Y.reshape(-1), V_x.reshape(test_len, -1), t0=t0, tN=tN,
+                          save=SAVE_ANIM,
+                          title="Data/Video/prediction_noTurb")
--- process_data.py
+++ process_data.py
@@ -4,29 +4,49 @@ import numpy as np
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
+    """
+    Loading Training Data from CSV
+    """
     start_idx = 0
-    end_idx = 384
-    path = "Data/Re200_NoTurbulence/Re200_Lam_NoTurb_"
+    end_idx = 20
+    path = "Data/Re200_WithTurbulence/Re200_Lam_wTurbulence_"
     X, Y, V_x, V_y, V_z, P = read_vortex_data(start_idx, end_idx, path)
 
     # SAVE_PLOT = None
     # tn = 180  # the nth snapshot, for plotting (verifying purpose only)
     # plot_vortex(X, Y, V_x, tn=tn, save=SAVE_PLOT)
-    #
-    # SAVE_ANIM = True
-    # t0 = 0  # the first frame to start animating
-    # tN = 100  # the last frame to stop animating
-    # anim = make_flow_anim(X, Y, V_x, t0=t0, tN=tN, save=SAVE_ANIM, title="VortexShedding_Re40_Laminar")
-
     """
     Making Training Data
     """
     # regularize the grid
-    V_x_reg, V_y_reg, _, _ = regularize_flow_data(X, Y, V_x, V_y)
+    V_x_reg, V_y_reg, X_reg, Y_reg = regularize_flow_data(X, Y, V_x, V_y)
     plt.imshow(V_x_reg[0].T, interpolation=None, cmap=plt.cm.get_cmap('RdBu_r'), alpha=1)
     plt.show()
 
-    # training data
-    training_data = np.stack([V_x_reg, V_y_reg], 1)
-    with open("Data/Processed/vortex_re200_no_turbulence.npy", 'wb') as f:
-        np.save(f, training_data)
+    # """
+    # Loading pre-processed training data from npy
+    # """
+    # processed_data = np.load("Data/Processed/vortex_re200_no_turbulence.npy")
+    # V_x = processed_data[:, 0, :, :]
+    # V_y = processed_data[:, 1, :, :]
+    #
+    # print(V_x.shape, X.shape)
+    # """
+    # Generating animation
+    # """
+    # SAVE_ANIM = True
+    # t0 = 0  # the first frame to start animating
+    # tN = 384  # the last frame to stop animating
+    # anim = make_flow_anim(X, Y, V_x, t0=t0, tN=tN,
+    #                       save=SAVE_ANIM,
+    #                       title="Data/Video/VortexShedding_Re200_noTurb")
+
+    # # save training data
+    # training_data = np.stack([V_x_reg, V_y_reg], 1)
+    # with open("Data/Processed/vortex_re200_with_turbulence.npy", 'wb') as f:
+    #     np.save(f, training_data)
+    print(X_reg.shape)
+    regularized_grid = np.stack([X_reg, Y_reg])
+    with open("Data/Processed/regularized_grid.npy", 'wb') as f:
+        np.save(f, regularized_grid)
+
