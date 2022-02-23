7d17e6cc67e2792edfcd14644b6c914e7df7b1dc
TomJZ <zjh@seas.upenn.edu>
2022-02-23 08:24:22 -0500

    saved two models

--- evaluation.py
+++ evaluation.py
@@ -6,40 +6,46 @@ if __name__ == "__main__":
     """
     load pretrained model
     """
-    ode_train = torch.load("SavedModels/vortex_conv_gaussian.pth",
+    ode_train = torch.load("SavedModels/vortex_conv_gaussian_noTurb.pth",
                            map_location=torch.device('cpu'))['ode_train']
 
     """
     Load initial condition from data 
     """
-    snapshot = 200  # the snapshot to use as initial condition
+    snapshot = 300  # the snapshot to use as initial condition
     processed_data = np.load("Data/Processed/vortex_re200_no_turbulence.npy")
     grid = np.load("Data/Processed/regularized_grid.npy")
     X = grid[0, 0, :, :]
     Y = grid[1, 0, :, :]
     initial_condition = torch.tensor(processed_data[snapshot]).unsqueeze(0)
-    print(initial_condition.shape)
+    print("Initial condition has shape: ", initial_condition.shape)
 
     """
     Generating predictions
     """
-    test_len = 50
-    step_skip = 5
+    test_len = 100  # length of prediction to generate
+    step_skip = 6  # number of steps within one time interval
     start = time.time()
+    # generating predictions
     z_p = ode_train(initial_condition,
                     step_skip * torch.tensor(np.arange(test_len).astype(int)),
                     return_whole_sequence=True)
     duration = time.time() - start
-    print("Total time taken to make prediction is: ", duration, " seconds")
     pred = z_p.detach().numpy()[:, 0, :, :]
+    print("Total time taken to make prediction is: ", duration, " seconds")
     print("Prediction has shape: ", pred.shape)
+    # predicted V_x and V_y
     V_x = pred[:, 0, :, :]
+    V_y = pred[:, 1, :, :]
+
+    """
+    Animating Predictions
+    """
     SAVE_ANIM = True
     SAVE_PLOT = None
     t0 = 0
     tN = t0 + test_len
 
-    print(X.shape, V_x.shape)
     anim = make_flow_anim(X.reshape(-1), Y.reshape(-1), V_x.reshape(test_len, -1), t0=t0, tN=tN,
                           save=SAVE_ANIM,
                           title="Data/Video/prediction_noTurb")
--- process_data.py
+++ process_data.py
@@ -4,49 +4,57 @@ import numpy as np
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    """
-    Loading Training Data from CSV
-    """
-    start_idx = 0
-    end_idx = 20
-    path = "Data/Re200_WithTurbulence/Re200_Lam_wTurbulence_"
-    X, Y, V_x, V_y, V_z, P = read_vortex_data(start_idx, end_idx, path)
-
-    # SAVE_PLOT = None
-    # tn = 180  # the nth snapshot, for plotting (verifying purpose only)
-    # plot_vortex(X, Y, V_x, tn=tn, save=SAVE_PLOT)
-    """
-    Making Training Data
-    """
-    # regularize the grid
-    V_x_reg, V_y_reg, X_reg, Y_reg = regularize_flow_data(X, Y, V_x, V_y)
-    plt.imshow(V_x_reg[0].T, interpolation=None, cmap=plt.cm.get_cmap('RdBu_r'), alpha=1)
-    plt.show()
-
     # """
-    # Loading pre-processed training data from npy
+    # Loading Training Data from CSV
     # """
-    # processed_data = np.load("Data/Processed/vortex_re200_no_turbulence.npy")
-    # V_x = processed_data[:, 0, :, :]
-    # V_y = processed_data[:, 1, :, :]
+    # start_idx = 0
+    # end_idx = 20
+    # path = "Data/Re200_WithTurbulence/Re200_Lam_wTurbulence_"
+    # X, Y, V_x, V_y, V_z, P = read_vortex_data(start_idx, end_idx, path)
     #
-    # print(V_x.shape, X.shape)
+    # # SAVE_PLOT = None
+    # # tn = 180  # the nth snapshot, for plotting (verifying purpose only)
+    # # plot_vortex(X, Y, V_x, tn=tn, save=SAVE_PLOT)
     # """
-    # Generating animation
+    # Making Training Data
     # """
-    # SAVE_ANIM = True
-    # t0 = 0  # the first frame to start animating
-    # tN = 384  # the last frame to stop animating
-    # anim = make_flow_anim(X, Y, V_x, t0=t0, tN=tN,
-    #                       save=SAVE_ANIM,
-    #                       title="Data/Video/VortexShedding_Re200_noTurb")
+    # # regularize the grid
+    # V_x_reg, V_y_reg, X_reg, Y_reg = regularize_flow_data(X, Y, V_x, V_y)
+    # plt.imshow(V_x_reg[0].T, interpolation=None, cmap=plt.cm.get_cmap('RdBu_r'), alpha=1)
+    # plt.show()
+
+    """
+    Loading pre-processed training data from npy
+    """
+    processed_data = np.load("Data/Processed/vortex_re200_no_turbulence.npy")
+    V_x = processed_data[:, 0, :, :].reshape(383, -1)
+    V_y = processed_data[:, 1, :, :]
+    processed_data2 = np.load("Data/Processed/vortex_re200_with_turbulence.npy")
+    V_x2 = processed_data2[:, 0, :, :].reshape(383, -1)
+    V_y2 = processed_data2[:, 1, :, :]
+
+    diff = V_x-V_x2
+    grid = np.load("Data/Processed/regularized_grid.npy")
+    X = grid[0, 0, :, :].reshape(-1)
+    Y = grid[1, 0, :, :].reshape(-1)
+
+    """
+    Generating animation
+    """
+    anim_data = diff
+    SAVE_ANIM = True
+    t0 = 100  # the first frame to start animating
+    tN = 200  # the last frame to stop animating
+    anim = make_flow_anim(X, Y, anim_data, t0=t0, tN=tN,
+                          save=SAVE_ANIM,
+                          title="Data/Video/turb_noturb_diff2")
 
     # # save training data
     # training_data = np.stack([V_x_reg, V_y_reg], 1)
     # with open("Data/Processed/vortex_re200_with_turbulence.npy", 'wb') as f:
     #     np.save(f, training_data)
-    print(X_reg.shape)
-    regularized_grid = np.stack([X_reg, Y_reg])
-    with open("Data/Processed/regularized_grid.npy", 'wb') as f:
-        np.save(f, regularized_grid)
+    # print(X_reg.shape)
+    # regularized_grid = np.stack([X_reg, Y_reg])
+    # with open("Data/Processed/regularized_grid.npy", 'wb') as f:
+    #     np.save(f, regularized_grid)
 
